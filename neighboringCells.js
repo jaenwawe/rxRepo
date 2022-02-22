9fb4b8c0d407c7dce818ec83a9abcc206d280d37
“Jae” <nwawe.jae@gmail.com>
2022-02-21 21:48:33 -0600

    check neighbors by row/col/living/selected

--- neighboringCells.js
+++ neighboringCells.js
@@ -8,14 +8,16 @@ const grid = []
  let cellOneObj= {
     row: center - 1, 
     col: center - 1,
-    living: isLive
+    living: isLive,
+    selected:false
    }
 grid.push(cellOneObj)
 
 let cellTwoObj= {
     row: center - 1, 
     col: center,
-    living: isLive
+    living: isLive,
+    selected:false
     }
 grid.push(cellTwoObj)
 
@@ -23,21 +25,24 @@ grid.push(cellTwoObj)
 let cellThreeObj= {
     row: center-1, 
     col: center+1,
-    living: isLive
+    living:true,
+    selected:false
     }
 grid.push(cellThreeObj)
 
 let cellFourObj= {
     row: center, 
     col: center-1,
-    living: isLive
+    living: isLive,
+    selected:false
     }
 grid.push(cellFourObj)
 
 let cellFiveObj= {
     row: center, 
     col: center,
-    living: isLive
+    living: isLive,
+    selected:false
     }
 grid.push(cellFiveObj)
 
@@ -45,7 +50,8 @@ grid.push(cellFiveObj)
 let cellSixObj= {
     row: center, 
     col: center+1,
-    living: isLive
+    living: true,
+    selected:false
     }
 grid.push(cellSixObj)
 
@@ -53,31 +59,62 @@ grid.push(cellSixObj)
 let cellSevenObj= {
     row: center +1, 
     col: center-1,
-    living: isLive
+    living: true,
+    selected:false
     }
 grid.push(cellSevenObj)
 
 let cellEightObj= {
     row: center +1, 
     col: center,
-    living: isLive
+    living: true,
+    selected:false
     }
 grid.push(cellEightObj)
 
 let cellNineObj= {
     row: center +1, 
     col: center+1,
-    living: isLive
+    living: true,
+    selected:false
     }
 grid.push(cellNineObj)
 
+let neighborCount = 0;
+for (cell of grid) {
+    let above = (cell.row=== center && cell.col=== center+1) 
+    let below = (cell.row=== center && cell.col=== center+1)
+    let rightRow = cell.row=== (center-1)
+    let leftRow =  cell.row=== (center+1)
 
 
+    if(rightRow && cell.living==true && cell.selected==false) {
+        neighborCount+=1;
+        cell.selected=true;
+    
+    }
+    if(leftRow && cell.living==true && cell.selected==false) {
+        neighborCount+=1;
+        cell.selected=true;
+    
+    }
+    if((above || below) && (cell.living==true && cell.selected==false)) {
+        neighborCount+=1;
+        cell.selected=true;
+    }
+}
+
+
+
+console.log('neighborCount =' + neighborCount)
 console.log(cellOneObj)
 console.log(grid)
 
 
+    // && cell.col===0) && cell.living === true && cell.selected === false) 
+    // neighborCount+=1;
+    // console.log(cell)
 
 
-
+//if((cell.row=== (0) && cell.col===0) && cell.living === true && cell.selected === false) 
 
